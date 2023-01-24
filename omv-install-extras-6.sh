fe28d8bf5644e48e609b56f8d60e85f2f08c6f95
Wolf2000 <support@wolf2000.at>
2023-01-24 18:48:01 +0100

    Update omv-install-extras-6.sh

--- omv-install-extras-6.sh
+++ omv-install-extras-6.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
+#Openmediavault 6
 echo
 echo -e "\033[36m Autoinstaller für Openmediavault 4.x und Extras.org\033[0m"
 echo
@@ -15,10 +16,42 @@ read answer
 echo  Ihre Antwort war: $answer
 # if [ "$answer" = "j" ]
 if [ "$answer" != "n" ]
-  then echo
-  
-  
-  
+  then echo apt-get install --yes gnupg &&
+wget -O "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc" https://packages.openmediavault.org/public/archive.key &&
+apt-key add "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc" &&
+
+cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
+deb https://packages.openmediavault.org/public shaitan main
+# deb https://downloads.sourceforge.net/project/openmediavault/packages shaitan main
+## Uncomment the following line to add software from the proposed repository.
+# deb https://packages.openmediavault.org/public shaitan-proposed main
+# deb https://downloads.sourceforge.net/project/openmediavault/packages shaitan-proposed main
+## This software is not part of OpenMediaVault, but is offered by third-party
+## developers as a service to OpenMediaVault users.
+# deb https://packages.openmediavault.org/public shaitan partner
+# deb https://downloads.sourceforge.net/project/openmediavault/packages shaitan partner
+EOF
+export LANG=C.UTF-8 &&
+export DEBIAN_FRONTEND=noninteractive &&
+export APT_LISTCHANGES_FRONTEND=none &&
+apt-get update &&
+apt-get --yes --auto-remove --show-upgraded \
+    --allow-downgrades --allow-change-held-packages \
+    --no-install-recommends \
+    --option DPkg::Options::="--force-confdef" \
+    --option DPkg::Options::="--force-confold" \
+    install openmediavault-keyring openmediavault &&
+omv-confdbadm populate &&
+apt-get update &&
+sleep 3 &&
+# non-free
+cd /root/
+sed -i -e "s/ main[[:space:]]*\$/ main contrib non-free/" /etc/apt/sources.list &&
+at update &&
+apt upgrade &&
+# OMV extras
+apt imstall wget &&
+wget -O - https://github.com/OpenMediaVault-Plugin-Developers/packages/raw/master/install | bash &&
   
 echo
 echo
