1e07dbbc0566815337c864cb31b7bfeaf8351fe2
Wolf2000 <support@wolf2000.at>
2023-01-28 00:11:18 +0100

    Update omv-install-extras-6.sh

--- omv-install-extras-6.sh
+++ omv-install-extras-6.sh
@@ -4,14 +4,14 @@
 
 #Openmediavault 6
 echo
-echo -e "\033[36m Autoinstaller für Openmediavault 4.x und Extras.org\033[0m"
-echo
+echo -e "\033[36m Autoinstaller für Openmediavault 6.x und Extras.org\033[0m"
+echo -e "\033[36m Autoinstaller for Openmediavault 6.x and Extras.org\033[0m"
 echo -e "\033[36m Author:     Wolf2000\033[0m"
-echo -e "\033[36m Version:         1.8\033[0m"
-echo -e "\033[36m https://forum-bpi.de\033[0m"
-echo -e "\033[31m Achtung das ist eine OMV-Test es kann eventuell nicht lauffähig sein \033[0m"
+echo -e "\033[36m Version:         2.1\033[0m"
+echo -e "\033[36m https://blog.wolf2000.at/[0m"
 echo
 echo -e "\033[32m Wolle sie Openmediaut mit Plugins installieren\033[0m"
+echo -e "\033[32m Want to install Openmediaut with plugins\033[0m"
 echo -e "\033[32m Ihre Antwort, n/j:\033[0m"
 read answer
 echo Das installieren wurde abgebrochen
@@ -114,5 +114,7 @@ apt upgrade --yes &&
 echo
 echo
 echo -e "\033[32m Das wars Openmediavult und Extras.org ist jetzt Installiert\033[0m"
+echo -e "\033[32m That's it Openmediavult and Extras.org is now installed\033[0m"
 else echo -e "\033[31m Die Installation wurde abgebrochen\033[0m"
+     echo -e "\033[31m The installation was aborted\033[0m"
 fi
