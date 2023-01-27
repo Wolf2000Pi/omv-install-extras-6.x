5813455be5f49eb079177bca426d0aa2f542fc56
Wolf2000 <support@wolf2000.at>
2023-01-27 21:10:07 +0100

    Update omv-install-extras-6.sh

--- omv-install-extras-6.sh
+++ omv-install-extras-6.sh
@@ -19,6 +19,7 @@ echo  Ihre Antwort war: $answer
 # if [ "$answer" = "j" ]
 if [ "$answer" != "n" ]
   then echo apt imstall wget &&
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" &&  
 apt-get install --yes gnupg &&
 wget -O "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc" https://packages.openmediavault.org/public/archive.key &&
 apt-key add "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc" &&
@@ -47,7 +48,7 @@ apt-get --yes --auto-remove --show-upgraded \
 	omv-confdbadm populate &&
 apt-get update &&
 sleep 3 &&
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" &&
+
 # OMV extras
 
 if [[ $(id -u) -ne 0 ]]; then
